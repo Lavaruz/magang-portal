13c93689f5ff48fbfdbe8c59b9ecf2abc08a8a66
Lavaruz <muzakiassami@gmail.com>
2023-11-08 21:14:13 +0700

    add recruiter page

--- src/router/viewRouter.ts
+++ src/router/viewRouter.ts
@@ -6,10 +6,13 @@ const router = express.Router();
 router.get("/",validateTokenWebsite, (req: Request, res: Response) => {
   res.render("SeekerProfilePage", {id: req.user.id});
 });
+router.get("/recruiter/profile",validateTokenWebsite, (req: Request, res: Response) => {
+  res.render("RecruiterProfilePage", {id: req.user.id});
+});
 router.get("/internships",validateTokenWebsite, (req, res) => {
   res.render("SeekerInternshipPage");
 });
-router.get("/recruiter-post",validateTokenWebsite, (req, res) => {
+router.get("/recruiter/post",validateTokenWebsite, (req, res) => {
   res.render("RecruiterPost");
 });
 
